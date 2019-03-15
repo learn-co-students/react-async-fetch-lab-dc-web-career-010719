f666697ce7f4fd27442639359b9bc9940dca4258
Ben Yellin <byellindc@gmail.com>
2019-03-15 15:54:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { PureComponent } from 'react'
+
+export default class App extends PureComponent {
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
